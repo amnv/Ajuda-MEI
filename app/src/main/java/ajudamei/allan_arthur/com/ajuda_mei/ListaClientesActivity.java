1b0649e72accd1581264e6632ddc552741166668
ajew <ajew@cin.ufpe.br>
2017-07-04 16:34:08 -0300

    commit 04/07 listVIew cliente funcionando

--- app/src/main/java/ajudamei/allan_arthur/com/ajuda_mei/ListaClientesActivity.java
+++ app/src/main/java/ajudamei/allan_arthur/com/ajuda_mei/ListaClientesActivity.java
@@ -52,10 +52,11 @@ public class ListaClientesActivity extends Activity {
         getGroupID_Cursor.close();
 
         List<Cliente> temp = getContatos(Integer.parseInt(groupID));
-        AdapterCliente adapter = new AdapterCliente(ListaClientesActivity.this, R.layout.dataregistro, temp);
+        AdapterCliente adapter = new AdapterCliente(ListaClientesActivity.this, R.layout.itemlista, temp);
 
         if(temp != null) {
             itens.setAdapter(adapter);
+            Toast.makeText(this,"Quantidade "+temp.size(),Toast.LENGTH_SHORT).show();
         }
     }
 
