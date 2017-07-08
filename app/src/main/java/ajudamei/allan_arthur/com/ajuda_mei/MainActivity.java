636ca2b00fa3976fdaf573a06d99da332845befe
ajew <ajew@cin.ufpe.br>
2017-07-08 16:35:05 -0300

    commit 08/07 sp do grupoCriado funcionando

--- app/src/main/java/ajudamei/allan_arthur/com/ajuda_mei/MainActivity.java
+++ app/src/main/java/ajudamei/allan_arthur/com/ajuda_mei/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -18,49 +19,25 @@ import ajudamei.allan_arthur.com.ajuda_mei.controle.estoque.TipoEstoqueActivity;
 import ajudamei.allan_arthur.com.ajuda_mei.empregados.EmpregadosActivity;
 import ajudamei.allan_arthur.com.ajuda_mei.lista.ListaPessoalActivity;
 
-public class MainActivity extends Activity {
-
-
-    public void checkContactsGroup(String nomeGrupo){
-        Cursor groupCursor;
-        String[] GROUP_PROJECTION = new String[] { ContactsContract.Groups._ID,     ContactsContract.Groups.TITLE };
-        groupCursor = this.getContentResolver().query(ContactsContract.Groups.CONTENT_URI,    GROUP_PROJECTION, ContactsContract.Groups.TITLE+ "=?", new String[]{nomeGrupo}, ContactsContract.Groups.TITLE + " ASC");
-
-        if(groupCursor.getCount() > 0){
-            Toast.makeText(MainActivity.this, "Group " + nomeGrupo+ " is already available", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        else {
-            Toast.makeText(MainActivity.this, "Not available", Toast.LENGTH_SHORT).show();
-
-            //Here we create a new Group
-            try {
-                ContentValues groupValues;
-                ContentResolver cr = this.getContentResolver();
-                groupValues = new ContentValues();
-                groupValues.put(ContactsContract.Groups.TITLE, nomeGrupo);
-                groupValues.put(ContactsContract.Groups.SHOULD_SYNC,true);
-                cr.insert(ContactsContract.Groups.CONTENT_URI, groupValues);
-            }
-            catch(Exception e){
-            }
-            Toast.makeText(MainActivity.this, "Created "+ nomeGrupo + " Successfully",Toast.LENGTH_SHORT).show();
-        }
-
-        groupCursor.close();
-
-    }
+public class MainActivity extends Activity{
+    private static final String MY_PREFS_NAME = "grupo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkContactsGroup("Clientes");
-        checkContactsGroup("Funcionários");
+        SharedPreferences settings = getSharedPreferences(MY_PREFS_NAME, 0);
+        boolean check = settings.getBoolean("criados", false);
+
+        if(!check){
+            checkContactsGroup("Clientes");
+            checkContactsGroup("Funcionários");
+            SharedPreferences setting = getSharedPreferences(MY_PREFS_NAME, 0);
+            SharedPreferences.Editor editor = setting.edit();
+            editor.putBoolean("criados", true);
+            editor.commit();
+        }
 
         final Button bt1 = (Button) findViewById(R.id.bt_controle_seu_estoque);
         bt1.setOnClickListener(new OnClickListener() {
@@ -120,4 +97,36 @@ public class MainActivity extends Activity {
         startActivity(a);
 
     }
+
+    public void checkContactsGroup(String nomeGrupo){
+        Cursor groupCursor;
+        String[] GROUP_PROJECTION = new String[] { ContactsContract.Groups._ID,     ContactsContract.Groups.TITLE };
+        groupCursor = this.getContentResolver().query(ContactsContract.Groups.CONTENT_URI,    GROUP_PROJECTION, ContactsContract.Groups.TITLE+ "=?", new String[]{nomeGrupo}, ContactsContract.Groups.TITLE + " ASC");
+
+        if(groupCursor.getCount() > 0){
+            Toast.makeText(MainActivity.this, "Group " + nomeGrupo+ " is already available", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Not available", Toast.LENGTH_SHORT).show();
+
+            //Here we create a new Group
+            try {
+                ContentValues groupValues;
+                ContentResolver cr = this.getContentResolver();
+                groupValues = new ContentValues();
+                groupValues.put(ContactsContract.Groups.TITLE, nomeGrupo);
+                groupValues.put(ContactsContract.Groups.SHOULD_SYNC,true);
+                cr.insert(ContactsContract.Groups.CONTENT_URI, groupValues);
+            }
+            catch(Exception e){
+            }
+            Toast.makeText(MainActivity.this, "Created "+ nomeGrupo + " Successfully",Toast.LENGTH_SHORT).show();
+        }
+
+        groupCursor.close();
+
+    }
+
+
 }
